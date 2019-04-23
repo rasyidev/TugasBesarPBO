09e9d81a6896d83f982b39859839d6925f770496
Habib Abdurrasyid <[habib.rasyid11@gmail.com]>
2019-04-23 17:03:29 +0700

    Create Laboran.java

---
+++ src/penjelasan/Laboran.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penjelasan;
+
+/**
+ *
+ * @author asus
+ */
+public class Laboran extends User{
+    
+    String username;
+    String password;
+
+    public Laboran(String username, String password, int id, String nama, String email, String jkel) {
+        super(id, nama, email, jkel);
+        this.username = username;
+        this.password = password;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    
+}
