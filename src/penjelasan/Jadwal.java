61f05721f02fb4d0eae0f83b00330efa7d247020
aldiindrawan <45248095+aldiindrawan@users.noreply.github.com>
2019-04-23 10:59:39 +0700

    class jadwal.java

---
+++ src/penjelasan/Jadwal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penjelasan;
+
+/**
+ *
+ * @author USER
+ */
+public class Jadwal {
+    int id_jadwal;
+    String prodi,matkul,tanggal,jam,kode_lab;
+    public void Jadwal(int id_jadwal,String prodi,String matkul,String tanggal,String jam,String kode_lab){
+        this.id_jadwal=id_jadwal;
+        this.prodi=prodi;
+        this.matkul
+        
+    }
+}
