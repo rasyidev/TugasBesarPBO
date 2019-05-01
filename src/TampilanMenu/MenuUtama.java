b9666acacb5fac612b414e55cf505c4b87b1c873
aldiindrawan <45248095+aldiindrawan@users.noreply.github.com>
2019-05-01 09:30:01 +0700

    tmbah form laboran

--- src/TampilanMenu/MenuUtama.java
+++ src/TampilanMenu/MenuUtama.java
@@ -117,7 +117,9 @@ public class MenuUtama extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+        Laboran x = new Laboran();
+        x.setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
